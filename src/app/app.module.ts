d2d60febfd5b1a6035db39a5316c8a732bf725a3
Gareth Clifford <gareth.clifford@live.co.uk>
2020-05-03 15:38:35 +0100

    GC - Adding pages and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+// For MDB Angular Free
+import { NavbarModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md';
+
 import { RoutingModule } from './routing/routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormMainComponent } from './components/form-main/form-main.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { NewsComponent } from './components/news/news.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormMainComponent,
+    ErrorPageComponent,
+    NewsComponent,
+    AboutMeComponent,
+    ContactMeComponent,
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    RoutingModule,
+    NavbarModule,
+    WavesModule,
+    ButtonsModule,
+    MDBBootstrapModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
