387babd2edac82a703f994ca1f9bc74284a5f062
Gareth Clifford <gareth.clifford@live.co.uk>
2020-05-17 12:34:23 +0100

    GC - Updating error page

--- src/app/components/error-page/error-page.component.ts
+++ src/app/components/error-page/error-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorPageComponent implements OnInit {
 
+  private router: RouterLink;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  button() {
+    console.log('thing', );
   }
 
 }
